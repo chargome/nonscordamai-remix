f884d10d7ca9555bd8a4128a4b61ef7a73c304bd
Charly Gomez <charly.gomez1310@gmail.com>
2022-11-14 18:14:10 +0100

    feat: auth layout

---
+++ app/components/Icons.tsx
@@ -0,0 +1,15 @@
+import { AiOutlineGoogle, AiOutlineGithub } from "react-icons/ai";
+import { CgMenuLeft } from "react-icons/cg";
+import { GiPizzaSlice } from "react-icons/gi";
+import { FaMapMarkerAlt, FaMap, FaCheck, FaTimes } from "react-icons/fa";
+
+export const Icons = {
+  Github: AiOutlineGithub,
+  Google: AiOutlineGoogle,
+  Menu: CgMenuLeft,
+  Pizza: GiPizzaSlice,
+  MapLocation: FaMap,
+  Location: FaMapMarkerAlt,
+  Check: FaCheck,
+  Cancel: FaTimes,
+};
---
+++ app/routes/__auth/login.tsx
@@ -0,0 +1,7 @@
+import LoginForm from "~/components/LoginForm";
+
+const LoginPage = (): JSX.Element => {
+  return <LoginForm />;
+};
+
+export default LoginPage;
